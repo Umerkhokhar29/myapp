f10931e5b82bf924e00dcc7d7d8fe804cc95bfa9
Umerkhokhar29 <muhammedumer292003@gmail.com>
2025-01-24 02:54:49 +0500

    connected backend to remote database

--- src/server.js
+++ src/server.js
@@ -1,22 +1,26 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");  
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require('dotenv').config();  
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", 
-  password: "nokia469", 
-  database: "user_data",
+  host: process.env.MYSQL_HOST,   
+  user: process.env.MYSQL_USER,   
+  password: process.env.MYSQL_PASSWORD, 
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  connectTimeout: 10000
 });
 
+// Connect to Railway MySQL
 db.connect((err) => {
   if (err) {
-    console.error("Error connecting to MySQL:", err.message);
+    console.error("Error connecting to MySQL:", err);
   } else {
     console.log("Connected to MySQL database.");
   }
@@ -41,27 +45,29 @@ app.post("/add-user", (req, res) => {
     }
   });
 });
-app.post("/delete-user", (req,res) => {
-    const {userId} = req.body;
 
-    const query = "DELETE from users_data_table where id = ? ";
-    db.query(query, [userId], (err,result)=> {
-        if(err){
-            console.error("Error deleting data:",err.message);
-            res.status(500).send("Failed to delete user.");
-        }
-        else if(result.affectedRows=0){
-            res.status(404).send("User not found.");
-        }
-        else{
-            res.status(200).send("User deleted successfully.")
-        }
-    }); 
+app.post("/delete-user", (req, res) => {
+  const { userId } = req.body;
 
-    
+  const query = "DELETE FROM users_data_table WHERE id = ?";
+  db.query(query, [userId], (err, result) => {
+    if (err) {
+      console.error("Error deleting data:", err.message);
+      res.status(500).send("Failed to delete user.");
+    } else if (result.affectedRows === 0) {
+      res.status(404).send("User not found.");
+    } else {
+      res.status(200).send("User deleted successfully.");
+    }
+  });
 });
 
 // Start the server
 app.listen(5000, () => {
   console.log("Server is running on port 5000.");
+  console.log('MySQL Host:', process.env.MYSQL_HOST);
+  console.log('MySQL User:', process.env.MYSQL_USER);
+  console.log('MySQL Database:', process.env.MYSQL_DATABASE);
+  console.log('MySQL Port:', process.env.MYSQL_PORT);
+
 });
